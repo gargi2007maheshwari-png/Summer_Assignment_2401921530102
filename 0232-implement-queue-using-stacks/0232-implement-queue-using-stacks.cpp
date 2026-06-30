23f500914625b20cf6b07142dd2b839282111f6a
gargi2007maheshwari-png <gargi2007maheshwari@gmail.com>
2026-06-30 22:08:47 +0530

    Time: 0 ms (100%), Space: 10.2 MB (28.69%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,40 @@
+class MyQueue {
+public:
+    stack<int> in;
+    stack<int> out;
+
+    MyQueue() {
+    }
+
+    void push(int x) {
+        in.push(x);
+    }
+
+    int pop() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        int front = out.top();
+        out.pop();
+        return front;
+    }
+
+    int peek() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        return out.top();
+    }
+
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
